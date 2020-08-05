22c7dc16d3f8863d74f7b4779367dd62dc062f0a
namgiho <namgiho96@gmail.com>
2020-08-05 15:44:35 +0900

    test04

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '희림아 사랑해~' });
+  res.render('index', { title: 'Test Debug' });
 });
 
 module.exports = router;
